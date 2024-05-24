4b354d41dc4d8571a7f90d03226e96b436d96999
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-05-24 13:09:30 -0400

    feat: Update slack-bridge to set user status based on incoming JSON data

--- netlify/functions/slack-bridge/slack-bridge.ts
+++ netlify/functions/slack-bridge/slack-bridge.ts
@@ -1,13 +1,30 @@
 import { Handler } from '@netlify/functions'
+import { App } from '@slack/bolt'
+import { ok } from 'assert'
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_OAUTH_TOKEN,
+})
 
 export const handler: Handler = async (event, context) => {
-  const json = JSON.parse(event.body!)
+  const json: { emoji: string, status: string, time: number } = JSON.parse(event.body!)
 
-  console.log(json.ello)
+  console.log(json)
+
+  const result = await app.client.users.profile.set({
+    token: process.env.SLACK_OAUTH_TOKEN,
+    profile: JSON.stringify({
+      status_text: json.status,
+      status_emoji: json.emoji,
+      status_expiration: json.time,
+    }),
+  })
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, ${json.ello}!`,
+      ok: result.ok,
     }),
   }
 }
